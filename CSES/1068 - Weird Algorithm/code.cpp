df3c5dc2b7fbe9597451d4bc61d0f30c98ed7583
SpencerLutz <spencerlutz1@gmail.com>
2023-05-30 12:02:57 -0700

    [CSES 1068] Update formatting

--- CSES/1068 - Weird Algorithm/code.cpp
+++ CSES/1068 - Weird Algorithm/code.cpp
@@ -7,8 +7,8 @@ int main() {
 
 	long long n; cin >> n;
     cout << n << " ";
-    while(n != 1) {
-        n = n % 2 ? 3 * n + 1 : n / 2;
+    while (n != 1) {
+        n = (n % 2) ? (3 * n + 1) : (n / 2);
         cout << n << " ";
     }
 }
