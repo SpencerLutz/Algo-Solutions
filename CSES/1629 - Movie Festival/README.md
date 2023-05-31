d9cef805db5a131879850af9ab123e8e52a8b30c
SpencerLutz <spencerlutz1@gmail.com>
2023-05-31 13:52:29 -0700

    [CSES 1629] Add solution

---
+++ CSES/1629 - Movie Festival/README.md
@@ -0,0 +1,1 @@
+# [1629 - Movie Festival](https://cses.fi/problemset/task/1629)
---
+++ CSES/1629 - Movie Festival/code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define trav(a, x) for (auto& a : x)
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    vector<pii> t(n);
+    rep(i,0,n) cin >> t[i].second >> t[i].first;
+    sort(all(t));
+    int ce = 0, ans = 0;
+    trav(p, t) if (p.second >= ce) {
+        ans++;
+        ce = p.first;
+    }
+    cout << ans;
+}
