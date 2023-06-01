bcd46396a2f4d2c072587e216b18a11e833eb217
SpencerLutz <spencerlutz1@gmail.com>
2023-05-31 19:09:15 -0700

    [CSES 1636] Add solution

---
+++ CSES/1636 - Coin Combinations II/README.md
@@ -0,0 +1,1 @@
+# [1636 - Coin Combinations II](https://cses.fi/problemset/task/1636)
---
+++ CSES/1636 - Coin Combinations II/code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define pem(x, y, m) (x = (x + y) % m)
+typedef vector<int> vi;
+
+int MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int w, n, x; cin >> n >> x;
+    vi dp(x + 1);
+    dp[0] = 1;
+    rep (i, 0, n) {
+        cin >> w;
+        rep (j, 0, x + 1 - w) 
+            pem(dp[j + w], dp[j], MOD);
+    }
+    cout << dp[x];
+}
